5efe257b46ab158fae8d5c92467f4eda646ae3e4
CJ2901 <christian.magallanes@alum.udep.edu.pe>
2021-11-22 00:48:44 -0500

    "inicio: app.js y clases creados"

---
+++ src/main/java/ejemplo/Persona.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ejemplo;
+
+/**
+ *
+ * @author Notes
+ */
+public class Persona {
+    
+    public static void personaHabla(){
+        System.out.println("Hola Mundo");
+    }
+    
+
+    
+}
