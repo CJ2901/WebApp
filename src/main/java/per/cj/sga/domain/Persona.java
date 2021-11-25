80c9b705806b872eae60207d6eab208fbd7a6bb8
CJ2901 <christian.magallanes@alum.udep.edu.pe>
2021-11-25 10:07:09 -0500

    "Capa DAO agregada"

--- src/main/java/per/cj/sga/domain/Persona.java
+++ src/main/java/per/cj/sga/domain/Persona.java
@@ -1,30 +1,45 @@
-
+/*
+Se convertirá en una clase de entidad para interactuar con el API de JPA hacia la base de datos
+*/
 package per.cj.sga.domain;
-
+ 
 import java.io.Serializable;
+import javax.persistence.*;
 
-// Como el cliente llamará esta clase de forma remota, se implementa un Serializable 
+// En la clase de Entity se pueden definir querys ("named queries")
+@Entity
+@NamedQueries({
+    @NamedQuery(name="Persona.findAll",query="SELECT p FROM Persona p ORDER BY p.idPersona") //No es SQL, es JPQL. Recupera objetos completos de la base de datos (NO COLUMNAS)
+})
+@Table(name="persona")
 public class Persona implements Serializable{
-    // 1 de 4 : Parametro que necesita la interface de Serializable
     private static final long serialVersionUID = 1L;
+    
+    // Agregamos los atributos de nuestra clase
+    
+    @Id
+    @GeneratedValue(strategy=GenerationType.IDENTITY)
+    @Column(name = "id_persona")
     private int idPersona;
+    
     private String nombre;
     private String apellido;
     private String email;
     private String telefono;
     
-    // 2 de 4: Se agregan los constructores con ayuda de Netbeans
-
-    public Persona(int idPersona, String nombre, String apellido, String email, String telefono) {
-        this.idPersona = idPersona;
+    // Se añade un constructor vacio
+    public Persona() {
+    }
+    
+    // Se añade un constructor que reciba todos los argumentos
+    public Persona(String nombre, String apellido, String email, String telefono) {
         this.nombre = nombre;
         this.apellido = apellido;
         this.email = email;
         this.telefono = telefono;
     }
     
-    // 3 de 4: Se agregan los métodos get y set con ayuda de Netbeans
-    
+    // Métodos get y set
     public int getIdPersona() {
         return idPersona;
     }
@@ -65,13 +80,11 @@ public class Persona implements Serializable{
         this.telefono = telefono;
     }
     
-    
-    // 4 de 4: Se agrega el método toString con ayuda de Netbeans
+    // Método tostring
 
     @Override
     public String toString() {
         return "Persona{" + "idPersona=" + idPersona + ", nombre=" + nombre + ", apellido=" + apellido + ", email=" + email + ", telefono=" + telefono + '}';
     }
-    
     
 }
